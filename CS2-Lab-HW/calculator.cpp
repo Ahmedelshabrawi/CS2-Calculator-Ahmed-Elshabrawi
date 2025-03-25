471a4c1d0f25e69a968dfab0c5c10cac52d455f2
Ahmedelshabrawi <Ahmedelshabrawi@aucegypt.edu>
2025-03-25 14:46:50 +0200

    Update calculator.cpp

--- CS2-Lab-HW/calculator.cpp
+++ CS2-Lab-HW/calculator.cpp
@@ -1,35 +1,39 @@
-double addition(double num1, double num2);
+
+double addition(double num1, double num2)
 {
     return num1+num2;
 }
-double subtraction(double num1,double num2);
+double subtraction(double num1,double num2)
 {
     return num1-num2;
 }
-double multiplication(double num1, double num2 );
+double multiplication(double num1, double num2 )
 {
     return num1*num2;
 }
-double division(double num1 ,double num2);
+double division(double num1 ,double num2)
 {
     return num1/num2;
 }
 int factorial (int num);
 {
-    return num*factorial(num-1);
+    if (num==1 )
+        return 1
+    else
+        return num*factorial(num-1);
 }
 int gcd(int num1, int num2 )
 {
     if (num2 !=0)
-        return gcd(num1,num1 %/num2 );
+        return gcd(num1,num1 %num2 )
     else
         return num1;
 }
-int random(int max, int min )
+int random(int min, int max )
 {
     int r;
     int num;
-    num=max-min+1
+    num=max-min+1;
     r=min+ rand()%num;
     return r;
 }
@@ -37,3 +41,4 @@ int lcm(int x, int y)
 {
         return (x / gcd(x, y)) * y;
 }
+
