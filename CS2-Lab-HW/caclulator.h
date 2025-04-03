bee955bb024fc2cfef31a68ae7c9451324f127d8
Nadinedonia <79872808+Nadinedonia@users.noreply.github.com>
2025-04-04 00:44:34 +0200

    bonus part caclulator.h

--- CS2-Lab-HW/caclulator.h
+++ CS2-Lab-HW/caclulator.h
@@ -6,3 +6,8 @@ int factorial (int num);
 int gcd(int num1,int num2);
 int lcm (int num1,int num2)
 int random(int num1, int num2);
+//BONUS PART
+double evaluateExpression(const std::string& expression);
+double addsub(const std::string& expression, size_t& index);
+double multdiv(const std::string& expression, size_t& index);
+double numpar(const std::string& expression, size_t& index);
